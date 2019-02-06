5171f0647601d13fc6ef32d96148abb3d1fff3dc
leco96 <a01206220@itesm.mx>
2019-02-06 16:00:06 -0600

    Terminar Lab1

---
+++ app/src/main/java/com/example/tec/lab1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.tec.lab1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button buttonPlay;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonPlay = (Button) findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this,Activity2.class);
+        startActivity(intent);
+    }
+
+}
